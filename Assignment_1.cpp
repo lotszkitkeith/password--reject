0ffa57284158da034e5595762cfb1d82fac794ed
sheldon cooper <coopsheldonfox@ucla.edu>
2013-02-02 17:37:36 -0800

    The original commit for the password project

---
+++ Assignment_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int  main()
+{
+   cout << "Hello World\n";
+   return 0;
+}
+
+
+/* -------Paste of run-------  
+   Hello World
+   Press any key to continue . . .
+   --------------------------*/
